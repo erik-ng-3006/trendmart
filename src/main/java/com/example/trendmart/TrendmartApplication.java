1668e8c98f3254dd8d86a794976b1d803a7273e9
erik-ng-3006 <erikng3006@gmail.com>
2024-12-29 20:11:54 +0700

    Init project and add necessary dependencies

---
+++ src/main/java/com/example/trendmart/TrendmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.trendmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendmartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trendmart/TrendmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trendmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
