42b2f60fbcd8471f74db19fc03af2b4680cb57d0
erik-ng-3006 <erikng3006@gmail.com>
2025-01-20 22:02:58 +0700

    setting relationship between product and category fix lombok error

--- src/main/java/com/example/trendmart/entities/Category.java
+++ src/main/java/com/example/trendmart/entities/Category.java
@@ -1,11 +1,19 @@
 package com.example.trendmart.entities;
 
-import jakarta.persistence.*;
-import lombok.*;
-import lombok.experimental.FieldDefaults;
-
 import java.util.List;
 import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
@@ -17,8 +25,13 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     UUID id;
+    
     String name;
 
     @OneToMany(mappedBy = "category")
     List<Product> products;
+
+    public Category(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/example/trendmart/entities/Product.java
+++ src/main/java/com/example/trendmart/entities/Product.java
@@ -1,12 +1,23 @@
 package com.example.trendmart.entities;
 
-import jakarta.persistence.*;
-import lombok.*;
-import lombok.experimental.FieldDefaults;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @Setter
 @Getter
@@ -35,4 +46,13 @@ public class Product {
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     List<Image> images;
+
+    public Product(String brand, String description, int inventory, String name, BigDecimal price, Category category) {
+        this.brand = brand;
+        this.category = category;
+        this.description = description;
+        this.inventory = inventory;
+        this.name = name;
+        this.price = price;
+    }
 }
