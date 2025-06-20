441381ffcb0e3cedb1dec4cd6fe3657cb0840c06
erik-ng-3006 <erikng3006@gmail.com>
2025-06-20 16:32:24 +0700

    Add JWT-based authentication and authorization
    - Introduced `JwtUtils`, `AuthTokenFilter`, and `JwtAuthEntryPoint` for token handling and error responses.
    - Updated `ShopConfig` to include JWT filter, exception handling, and authentication provider configuration.
    - Added JWT-related configurations in `application.properties`.
    - Rolled back `jjwt` dependencies to version 0.11.5 for compatibility.

--- src/main/java/com/example/trendmart/security/config/ShopConfig.java
+++ src/main/java/com/example/trendmart/security/config/ShopConfig.java
@@ -1,14 +1,74 @@
 package com.example.trendmart.security.config;
 
+import com.example.trendmart.security.jwt.AuthTokenFilter;
+import com.example.trendmart.security.jwt.JwtAuthEntryPoint;
+import com.example.trendmart.security.user.ShopUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
+
 
 @Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class ShopConfig {
+    private final ShopUserDetailsService shopUserDetailsService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
+    private static final List<String> SECURED_URLS = List.of(
+            "/api/v1/carts/**", "/api/v1/cartItems/**");
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        var AuthenticationProvider = new DaoAuthenticationProvider();
+        AuthenticationProvider.setUserDetailsService(shopUserDetailsService);
+        AuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return AuthenticationProvider;
+    }
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(SECURED_URLS.toArray(String[]::new)).permitAll()
+                );
+        http.authenticationProvider(daoAuthenticationProvider());
+        http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
